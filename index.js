25925462184c4c7f9d1419cda987d7a1884fd3bb
Steve <stevegardiner26@aol.com>
2020-07-04 16:12:51 -0400

    JS Progress

--- index.js
+++ index.js
@@ -23,3 +23,44 @@
 
 // Check localStorage for a start_time if there is none create one.
 // Check if the time is being currently tracked and if so display hours.
+
+var start_btn = document.getElementById("start");
+var pause_btn = document.getElementById("pause");
+var resume_btn = document.getElementById("resume");
+var stop_btn = document.getElementById("stop");
+var status_text = document.getElementsByClassName("status")[0];
+
+if (!localStorage.tracking || localStorage.tracking == false) {
+    if (!localStorage.start_time) {
+        start_btn.style.display = "block";
+    } else {
+        resume_btn.style.display = "block";
+    }
+}
+
+// Update Status Text
+// Display Proper Buttons
+// Set Proper LocalStorage vars
+// Update display hours/mins
+
+
+function before_time_start () {
+
+}
+
+function finish_time () {
+
+}
+
+function pause_time () {
+
+}
+
+function track_time () {
+
+}
+
+start_btn.addEventListener("click", function () {
+    localStorage.tracking = true;
+    localStorage.start_time = new Date();
+});
